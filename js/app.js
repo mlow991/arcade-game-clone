29e33fe60aa1a272a735e600c44c42f96585769e
mlow991 <m.low91@gmail.com>
2015-07-08 14:23:32 -0700

    Implemented random number gen to be used for starting lane, and speed

--- js/app.js
+++ js/app.js
@@ -7,6 +7,13 @@ Entity.prototype.init = function(loc) {
     this.y = loc[1];
 }
 
+/* Random number generator code reference:
+   https://developer.mozilla.org/en-US/docs/Web/
+   JavaScript/Reference/Global_Objects/Math/random 
+*/
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // Enemies our player must avoid
 var Enemy = function() {
